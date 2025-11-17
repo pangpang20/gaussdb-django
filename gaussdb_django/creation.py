276e1fe317ebbb77544bcee00417d61a9af30c0e
pangpang20 <676814828@qq.com>
2025-11-17 20:59:18 +0800

    Add proper BSD-3-Clause license headers and attribution notices across all files, update LICENSE with original Django text, and bump version to 5.2.1

--- gaussdb_django/creation.py
+++ gaussdb_django/creation.py
@@ -1,3 +1,16 @@
+"""
+GaussDB schema module for Django.
+This module incorporates code from the Django project, which is
+licensed under the three-clause BSD license.
+Copyright (c) Django Software Foundation and individual contributors.
+All rights reserved.
+This derivative work is licensed under the same BSD license.
+Copyright (c) 2025, HuaweiCloudDeveloper
+All rights reserved.
+For more information about Django's license, see the LICENSE file in the
+root directory of this distribution.
+"""
+
 import sys
 
 from django.core.exceptions import ImproperlyConfigured
